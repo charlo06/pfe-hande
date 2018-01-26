d0033029af9ced62a7c5ebd903a71c08a9a11e3b
Charlélie Obadia <charlelieob@gmail.com>
2018-01-26 10:15:22 +0100

    updated code to accelerometer governor

--- algo/accelerometre/accelerometre.ino
+++ algo/accelerometre/accelerometre.ino
@@ -3,9 +3,10 @@
 #include <Servo.h>
 
 /* Créer un objet Servo pour contrôler le servomoteur */
-Servo monServomoteur, servo2;
+Servo servoX, servoY, servoZ;
     const int MPU_addr=0x68;  // I2C address of the MPU-6050
     int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+    int initX, initY,initZ;
     void setup(){
       Wire.begin();
       Wire.beginTransmission(MPU_addr);
@@ -15,8 +16,20 @@ Servo monServomoteur, servo2;
       Serial.begin(9600);
 
         // Attache le servomoteur à la broche D9
-        monServomoteur.attach(9);
-        servo2.attach(10);
+        servoX.attach(9);
+        servoY.attach(10);
+        servoZ.attach(8);
+       // monServomoteur.attach(9);
+       // servo2.attach(10);
+
+        Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+      initX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+      initY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+      initZ=Wire.read()<<8|Wire.read();
+
+      //servoX.write(90);
+      servoY.write(90);
+      servoZ.write(90);
     }
     void loop(){
       Wire.beginTransmission(MPU_addr);
@@ -27,9 +40,17 @@ Servo monServomoteur, servo2;
       AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
       AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
    //   Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    //  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    //  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-     // GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+      GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+      GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+      GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+      //AcX= AcX -initX;
+      AcY= AcY -300;
+      AcZ= AcZ+1500;
+
+      GyX =(GyX + 3380)/10;
+      GyY = (GyY - 990)/10;
+      GyZ = (GyZ  +210)/10;
       Serial.print("AcX = "); Serial.print(AcX);
       Serial.print(" | AcY = "); Serial.print(AcY);
       Serial.print(" | AcZ = "); Serial.print(AcZ);
@@ -37,15 +58,14 @@ Servo monServomoteur, servo2;
       Serial.print(" | GyX = "); Serial.print(GyX);
       Serial.print(" | GyY = "); Serial.print(GyY);
       Serial.print(" | GyZ = "); Serial.println(GyZ);
+      int value1 = constrain(GyY,-180,180);
+      value1= map (value1,-180,180,60,120);
+      servoY.write(value1);
+      int value2 = constrain(GyZ,-180,180);
+      value2=map(value2,-180,180,60,120);
+      servoZ.write(value2);
 
-      int value1 = constrain(AcX,-180,180);
-      value1= map (value1,-180,180,0,180);
-      monServomoteur.write(value1);
-      int value2 = constrain(AcY,-180,180);
-      value2=map(value2,-180,180,0,180);
-      servo2.write(value2);
-
-      delay(5);
+      delay(50);
       
         // Fait bouger le bras de 0° à 180°
      /* for (int position = 0; position <= 180; position++) {
