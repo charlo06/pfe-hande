10525f64cc446c06be1f6333fd1c970179067e4c
Charlélie Obadia <charlelieob@gmail.com>
2018-02-01 18:01:18 +0100

    updated android app to display graph

--- HandEManager/app/src/main/java/fr/ece/hande/handemanager/MainActivity.java
+++ HandEManager/app/src/main/java/fr/ece/hande/handemanager/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.ece.hande.handemanager;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        ImageButton imageButton = (ImageButton) view;
+
+        switch (imageButton.getId()){
+            case R.id.imgButtonContact:
+                System.out.println("contact");
+                break;
+            case R.id.imgButtonReport:
+                System.out.println("report");
+                Intent intent =new Intent (this,GraphDataActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.imgButtonTraining:
+                System.out.println("training");
+                Intent intent2 = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://www.stackoverflow.com"));
+                startActivity(intent2);
+                break;
+        }
     }
 }
