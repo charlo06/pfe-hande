34a88e73889e06dcc1240d9402083a57f598423a
Charlélie Obadia <charlelieob@gmail.com>
2018-02-01 15:49:20 +0100

    update accelerometer

---
+++ HandEManager/app/src/main/java/fr/ece/hande/handemanager/BTActivity.java
@@ -0,0 +1,13 @@
+package fr.ece.hande.handemanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bt);
+    }
+}
